c467730d8349448dca0da4a86ece58f33de61931
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-09-18 20:11:02 +0300

    init

--- api/telegram/start.go
+++ api/telegram/start.go
@@ -1,6 +1,7 @@
 package telegram
 
 import (
+	"fmt"
 	"github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
@@ -12,12 +13,10 @@ func NewStartHandler() Handler {
 }
 
 func (s StartHandler) Handle(req *tgbotapi.Message) (resp tgbotapi.MessageConfig, err error) {
-	resp = tgbotapi.NewMessage(req.Chat.ID, "Welcome to the Awakari bot!")
+	resp = tgbotapi.NewMessage(req.Chat.ID, fmt.Sprintf("Let's create a new subscription here for \"%s\"", req.Chat.Title))
 	menu := tgbotapi.NewInlineKeyboardMarkup(
-		tgbotapi.NewInlineKeyboardRow(tgbotapi.NewInlineKeyboardButtonData("New Subscription", "button1")),
-		tgbotapi.NewInlineKeyboardRow(tgbotapi.NewInlineKeyboardButtonData("Edit Subscription", "button2")),
-		tgbotapi.NewInlineKeyboardRow(tgbotapi.NewInlineKeyboardButtonData("Delete Subscription", "button3")),
-		tgbotapi.NewInlineKeyboardRow(tgbotapi.NewInlineKeyboardButtonData("New Custom Message", "button4")),
+		tgbotapi.NewInlineKeyboardRow(tgbotapi.NewInlineKeyboardButtonData("Create Subscription", "button1")),
+		tgbotapi.NewInlineKeyboardRow(tgbotapi.NewInlineKeyboardButtonData("Send Custom Message", "button2")),
 	)
 	resp.ReplyMarkup = menu
 	return
