f8bc950fd4627588aee23253bbfd9fb2765bd036
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-09-19 19:11:49 +0300

    init

--- api/telegram/start.go
+++ api/telegram/start.go
@@ -6,44 +6,32 @@ import (
 	"gopkg.in/telebot.v3"
 )
 
-type startHandler struct {
-	bot *telebot.Bot
-}
-
 var ErrChatType = errors.New("unsupported chat type (supported options: \"group\", \"private\")")
 
-func NewStartHandler(bot *telebot.Bot) Handler {
-	return startHandler{
-		bot: bot,
+func Start(ctx telebot.Context) (err error) {
+	chat := ctx.Chat()
+	switch chat.Type {
+	case telebot.ChatGroup:
+		err = startSubscription(ctx, chat.ID)
+	case telebot.ChatPrivate:
+		err = startMessage(ctx, chat.ID)
+	default:
+		err = fmt.Errorf("%w: %s", ErrChatType, chat.Type)
 	}
+	return
 }
 
-func (h startHandler) Handler() telebot.HandlerFunc {
-	return func(ctx telebot.Context) (err error) {
-		chat := ctx.Chat()
-		switch chat.Type {
-		case telebot.ChatGroup:
-			err = h.startSubscription(ctx, chat.ID)
-		case telebot.ChatPrivate:
-			err = h.startMessage(ctx, chat.ID)
-		default:
-			err = fmt.Errorf("%w: %s", ErrChatType, chat.Type)
-		}
-		return
-	}
-}
-
-func (h startHandler) startSubscription(ctx telebot.Context, chatId int64) (err error) {
-	markup := h.bot.NewMarkup()
-	markup.Inline(telebot.Row{
-		markup.WebApp("Open", &telebot.WebApp{
-			URL: "https://google.com",
-		}),
-	})
+func startSubscription(ctx telebot.Context, chatId int64) (err error) {
+	markup := &telebot.ReplyMarkup{}
+	markup.Inline(
+		telebot.Row{
+			telebot.Btn{Text: "Set up new subscription", WebApp: &telebot.WebApp{URL: "https://google.com"}},
+		},
+	)
 	return ctx.Send("Open this app!", markup)
 }
 
-func (h startHandler) startMessage(ctx telebot.Context, chatId int64) (err error) {
+func startMessage(ctx telebot.Context, chatId int64) (err error) {
 	fmt.Printf("TODO: start message")
 	return
 }
