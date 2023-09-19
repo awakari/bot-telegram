c53a75e7cb99731a99af813de14ed3c6b2bf351e
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-09-19 17:36:52 +0300

    init

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/stretchr/testify v1.8.4
-	golang.org/x/exp v0.0.0-20230905200255-921286631fa9
+	gopkg.in/telebot.v3 v3.1.3
 )
 
 require (
--- main.go
+++ main.go
@@ -3,12 +3,8 @@ package main
 import (
 	"crypto/tls"
 	"fmt"
-	"github.com/awakari/bot-telegram/api/telegram/update"
-	"github.com/awakari/bot-telegram/api/telegram/update/message"
-	"github.com/awakari/bot-telegram/api/telegram/update/message/command"
-	"github.com/awakari/bot-telegram/api/telegram/update/query"
 	"github.com/awakari/bot-telegram/config"
-	"github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"gopkg.in/telebot.v3"
 	"log/slog"
 	"net/http"
 	"os"
@@ -26,62 +22,31 @@ func main() {
 	}
 	log := slog.New(slog.NewTextHandler(os.Stdout, &opts))
 	//
-	bot, err := tgbotapi.NewBotAPIWithClient(cfg.Api.Token, tgbotapi.APIEndpoint, &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	s := telebot.Settings{
+		Client: &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			},
 		},
+		Token: cfg.Api.Token,
+		Poller: &telebot.Webhook{
+			Listen:        fmt.Sprintf(":%d", cfg.Api.Port),
+			HasCustomCert: true,
+			Endpoint: &telebot.WebhookEndpoint{
+				PublicURL: fmt.Sprintf("https://%s%s", cfg.Api.Host, cfg.Api.Path),
+				Cert:      "/etc/server-cert/tls.crt",
+			},
+		},
+		Verbose: true,
+	}
+	var b *telebot.Bot
+	b, err = telebot.NewBot(s)
+	if err != nil {
+		panic(err)
+	}
+	b.Handle(telebot.OnText, func(ctx telebot.Context) error {
+		log.Info(fmt.Sprintf("update: %+v", ctx.Update()))
+		return ctx.Send(ctx.Text())
 	})
-	if err != nil {
-		panic(err)
-	}
-	bot.Debug = true
-	log.Info(fmt.Sprintf("Authorized on account %s", bot.Self.UserName))
-	certData, err := os.ReadFile("/etc/server-cert/tls.crt")
-	if err != nil {
-		panic(err)
-	}
-	certFile := tgbotapi.FileBytes{
-		Name:  "server-cert",
-		Bytes: certData,
-	}
-	wh, _ := tgbotapi.NewWebhookWithCert(fmt.Sprintf("https://%s%s", cfg.Api.Host, cfg.Api.Path), certFile)
-	_, err = bot.Request(wh)
-	if err != nil {
-		panic(err)
-	}
-	info, err := bot.GetWebhookInfo()
-	if err != nil {
-		panic(err)
-	}
-	if info.LastErrorDate != 0 {
-		panic(err)
-	}
-	log.Info(fmt.Sprintf("Webhook listen path: %s", cfg.Api.Path))
-	chUpdates := bot.ListenForWebhook(cfg.Api.Path)
-	//
-	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
-	})
-	//
-	go http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", cfg.Api.Port), nil)
-	//
-	startCmdHandler := command.NewStartCommandHandler()
-	cmdHandlerByCmd := map[string]command.Handler{
-		"start": startCmdHandler,
-	}
-	cmdHandler := command.NewHandler(cmdHandlerByCmd)
-	cmdHandler = command.NewLoggingHandler(cmdHandler, log)
-	msgHandler := message.NewHandler(cmdHandler)
-	msgHandler = message.NewLoggingHandler(msgHandler, log)
-	msgHandler = message.NewErrorHandler(msgHandler)
-	queryHandler := query.NewHandler()
-	queryHandler = query.NewLoggingHandler(queryHandler, log)
-	queryHandler = query.NewErrorHandler(queryHandler)
-	updHandler := update.NewHandler(bot, msgHandler, queryHandler)
-	updHandler = update.NewLoggingHandler(updHandler, log)
-	log.Info("Start processing updates...")
-	for u := range chUpdates {
-		_ = updHandler.Handle(u)
-	}
+	b.Start()
 }
