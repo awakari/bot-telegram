dae94a396104fee615012be434274fd6a0cf8c92
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-09-20 12:34:19 +0300

    init

--- main.go
+++ main.go
@@ -46,6 +46,7 @@ func main() {
 		},
 		Token: cfg.Api.Telegram.Token,
 	}
+	log.Info(fmt.Sprintf("Telegram bot settigs: %+v", s))
 	var b *telebot.Bot
 	b, err = telebot.NewBot(s)
 	if err != nil {
