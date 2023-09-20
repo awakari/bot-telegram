40cc8ffa4591aaab5e688e282ed23765716e455a
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-09-20 08:29:43 +0300

    init

--- api/telegram/start.go
+++ api/telegram/start.go
@@ -6,12 +6,11 @@ import (
 	"gopkg.in/telebot.v3"
 )
 
-const msgStartGroup = "Select a subscription from the list"
-const msgStartPrivate = `
-* Just type a text to send a simple text message to Awakari.
-* Send "/sub name keyword1 keyword2 ..." command to create a basic text matching subscription.
-* To customize a message or a subscription, choose a button below.
-`
+const msgStartGroup = "Here follows the list of your subscriptions. Select any to proceed."
+
+const msgStartPrivate = "* Just type a text to send a simple text message to Awakari.\n" +
+	"* Send a `/sub name keyword1 keyword2 ...` command to create a basic text matching subscription.\n" +
+	"* To customize a message or a subscription, choose a button below."
 
 var ErrChatType = errors.New("unsupported chat type (supported options: \"group\", \"private\")")
 
@@ -68,6 +67,6 @@ func startGroup(ctx telebot.Context, chatId int64) (err error) {
 func startPrivate(ctx telebot.Context, chatId int64) (err error) {
 	m := &telebot.ReplyMarkup{}
 	m.Reply(m.Row(btnSubNewCustom, btnMsgNewCustom))
-	err = ctx.Send(msgStartPrivate, m, telebot.ModeMarkdownV2)
+	err = ctx.Send(msgStartPrivate, m, telebot.ModeMarkdown)
 	return
 }
