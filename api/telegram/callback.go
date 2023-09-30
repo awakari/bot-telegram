60f10897870554cfa11afb1ded0b95ad1c417c69
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-09-30 08:44:54 +0300

    save

--- api/telegram/callback.go
+++ api/telegram/callback.go
@@ -14,14 +14,14 @@ func Callback(handlers map[string]func(ctx telebot.Context, args ...string) (err
 	return func(ctx telebot.Context) (err error) {
 		data := ctx.Callback().Data
 		parts := strings.Split(data, " ")
-		if len(parts) != 2 {
+		if len(parts) < 2 {
 			err = fmt.Errorf("%w: %s", errInvalidCallbackData, data)
 		}
-		var arg string
+		var args []string
 		var f func(ctx telebot.Context, args ...string) (err error)
 		if err == nil {
 			cmd := parts[0]
-			arg = parts[1]
+			args = parts[1:]
 			var ok bool
 			f, ok = handlers[cmd]
 			if !ok {
@@ -29,7 +29,7 @@ func Callback(handlers map[string]func(ctx telebot.Context, args ...string) (err
 			}
 		}
 		if err == nil {
-			err = f(ctx, arg)
+			err = f(ctx, args...)
 		}
 		return
 	}
