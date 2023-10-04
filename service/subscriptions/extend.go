d99311294b26d31eed8f1199b077cd4bfce04567
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-10-04 20:03:33 +0300

    save

--- service/subscriptions/extend.go
+++ service/subscriptions/extend.go
@@ -75,7 +75,7 @@ func ExtendReplyHandlerFunc(paymentProviderToken string, kbd *telebot.ReplyMarku
 				Token: paymentProviderToken,
 				Total: int(float64(countDays) * pricePerDay * service.SubCurrencyFactor),
 			}
-			_, err = tgCtx.Bot().Send(tgCtx.Sender(), &invoice, kbd)
+			_, err = tgCtx.Bot().Send(tgCtx.Sender(), &invoice)
 		}
 		return
 	}
