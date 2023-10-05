d429a41bf19375aec47f26029be37870ff3fb58e
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-10-05 18:54:40 +0300

    save

--- config/config_test.go
+++ config/config_test.go
@@ -10,10 +10,10 @@ func TestConfig(t *testing.T) {
 	os.Setenv("LOG_LEVEL", "4")
 	os.Setenv("API_TELEGRAM_WEBHOOK_PORT", "56789")
 	os.Setenv("API_TELEGRAM_TOKEN", "yohoho")
-	os.Setenv("API_PAYMENT_PROVIDER_TOKEN", "yohoho")
+	os.Setenv("PAYMENT_PROVIDER_TOKEN", "yohoho")
 	cfg, err := NewConfigFromEnv()
 	assert.Nil(t, err)
 	assert.Equal(t, uint16(56789), cfg.Api.Telegram.Webhook.Port)
 	assert.Equal(t, 4, cfg.Log.Level)
-	assert.Equal(t, "yohoho", cfg.Api.PaymentProviderToken)
+	assert.Equal(t, "yohoho", cfg.Payment.Provider.Token)
 }
