d436a36b15dbcec32d6062ebef9b0e57de6b4c0f
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-10-13 11:10:39 +0300

    fix a stop message

--- service/subscriptions/stop.go
+++ service/subscriptions/stop.go
@@ -18,7 +18,7 @@ func Stop(chatStor chats.Storage) service.ArgHandlerFunc {
 		}
 		err = chatStor.UnlinkSubscription(context.Background(), k)
 		if err == nil {
-			_ = tgCtx.Send(fmt.Sprintf("Unlinked the subscription from this chat.Note: don't delete this group for up to next %s. Some new messages may appear here.", chats.ReaderTtl))
+			_ = tgCtx.Send(fmt.Sprintf("Unlinked the subscription from this chat. Note: don't delete this group for the next %s. Some new messages may appear here.", chats.ReaderTtl))
 		}
 		return
 	}
