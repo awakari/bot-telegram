180e236cfee74357e8ab89f2c1c6356683ae4500
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-10-18 19:49:43 +0300

    rework

--- service/messages/details.go
+++ service/messages/details.go
@@ -14,11 +14,25 @@ import (
 func DetailsHandlerFunc(clientAwk api.Client, groupId string) telebot.HandlerFunc {
 	return func(tgCtx telebot.Context) (err error) {
 		//
+		if err == nil {
+			m := &telebot.ReplyMarkup{}
+			m.Inline(m.Row(
+				telebot.Btn{
+					Text: "Own",
+				},
+				telebot.Btn{
+					Text: "All",
+				},
+			))
+			err = tgCtx.Send("List Sources:", m)
+		}
+		//
 		groupIdCtx := metadata.AppendToOutgoingContext(context.TODO(), "x-awakari-group-id", groupId)
 		userId := strconv.FormatInt(tgCtx.Sender().ID, 10)
-		//
 		var u awkUsage.Usage
-		u, err = clientAwk.ReadUsage(groupIdCtx, userId, awkUsage.SubjectPublishEvents)
+		if err == nil {
+			u, err = clientAwk.ReadUsage(groupIdCtx, userId, awkUsage.SubjectPublishEvents)
+		}
 		var l awkUsage.Limit
 		if err == nil {
 			l, err = clientAwk.ReadUsageLimit(groupIdCtx, userId, awkUsage.SubjectPublishEvents)
@@ -31,18 +45,6 @@ func DetailsHandlerFunc(clientAwk api.Client, groupId string) telebot.HandlerFun
 				Data: fmt.Sprintf("%s %d", usage.CmdLimit, awkUsage.SubjectPublishEvents),
 			}))
 			err = tgCtx.Send(respTxt, m, telebot.ModeHTML)
-		}
-		if err == nil {
-			m := &telebot.ReplyMarkup{}
-			m.Inline(m.Row(
-				telebot.Btn{
-					Text: "Own",
-				},
-				telebot.Btn{
-					Text: "Common",
-				},
-			))
-			err = tgCtx.Send("List Sources:", m)
 		}
 		//
 		return
