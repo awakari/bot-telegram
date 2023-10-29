a5fbb5451c323889c0fac984cf8edfaa08d965f8
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-10-29 20:35:12 +0200

    rework limits expiration time extension

--- service/usage/order.go
+++ service/usage/order.go
@@ -15,8 +15,8 @@ type OrderLimit struct {
 
 const orderLimitCountMinMsgs = 11
 const orderLimitCountMinSubs = 2
-const orderLimitCountMaxSubs = 100000
-const orderLimitCountMaxMsgs = 1000
+const orderLimitCountMaxSubs = 1000
+const orderLimitCountMaxMsgs = 100000
 
 var errInvalidOrder = errors.New("invalid order")
 
