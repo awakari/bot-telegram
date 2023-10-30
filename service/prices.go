eab56bd06dc5d93933bdd03e29bee96c991604e5
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-10-30 17:31:40 +0200

    + /prices command

--- service/prices.go
+++ service/prices.go
@@ -13,17 +13,17 @@ type PricesHandler struct {
 
 const fmtMsgPrices = `
 **Always Free**:
-* 1 subscription that never expires. 
-* Publish up to 10 messages daily.
-* Adding own publishing sources.
+✅ 1 subscription that never expires. 
+✅ Publish up to 10 messages daily.
+✅ Adding own publishing sources.
 
 **Prices**:
-* Custom Usage Limit:
-	* Every subscription starting from the 2nd: %s %.2f per day
-	* Every message publication starting from 11th: %s %.2f per day
-* On Demand:
-	* A subscription extension: %s %.2f per day 
-	* A message publication when limit is reached: %s %.2f
+- Custom Usage Limit:
+	- Every subscription starting from the 2nd: %s %.2f per day
+	- Every message publication starting from 11th: %s %.2f per day
+- On Demand:
+	- A subscription extension: %s %.2f per day 
+	- A message publication when limit is reached: %s %.2f
 `
 
 func (ph PricesHandler) Prices(tgCtx telebot.Context) (err error) {
