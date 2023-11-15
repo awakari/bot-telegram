4b9e52379c43126d7e8d7c28c8551edffa6a9fa5
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-11-15 18:46:44 +0200

    save

--- service/menu.go
+++ service/menu.go
@@ -14,7 +14,7 @@ const LabelPubMsgCustom = "▷ Custom"
 const LabelUsagePub = "Usage ᵖ"
 const LabelMainMenu = "< Main Menu"
 
-const LabelPub = "▶✉🖄🖃 Publish"
+const LabelPub = "📢 Publish"
 const LabelSub = "⭐ Subscribe"
 
 var btnSubList = telebot.Btn{
