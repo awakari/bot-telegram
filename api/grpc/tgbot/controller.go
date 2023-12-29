9918a7007b1bbe4b0396d855565e40d248edd036
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-12-29 21:34:22 +0200

    feat: list of channels being read

--- api/grpc/tgbot/controller.go
+++ api/grpc/tgbot/controller.go
@@ -3,7 +3,6 @@ package tgbot
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"github.com/awakari/bot-telegram/service/messages"
 	tgverifier "github.com/electrofocus/telegram-auth-verifier"
 	"google.golang.org/grpc/codes"
@@ -41,7 +40,6 @@ func (c controller) Authenticate(ctx context.Context, req *AuthenticateRequest) 
 }
 
 func (c controller) ListChannels(ctx context.Context, req *ListChannelsRequest) (resp *ListChannelsResponse, err error) {
-	fmt.Printf("api.grpc.tgbot.ListChannels(%+v)\n", req)
 	resp = &ListChannelsResponse{
 		Page: []*Channel{},
 	}
@@ -70,7 +68,6 @@ func (c controller) ListChannels(ctx context.Context, req *ListChannelsRequest) 
 		}
 	}
 	err = encodeError(err)
-	fmt.Printf("api.grpc.tgbot.ListChannels(...): resp=%+v, err=%s\n", resp, err)
 	return
 }
 
